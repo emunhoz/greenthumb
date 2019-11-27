025100afd0ec2278b9263b348cddc14d27a91576
Eder <edermunhozsantos@gmail.com>
2019-11-27 02:56:54 -0300

    fix: storybook with button example

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,17 +1,4 @@
-import React from 'react'
-import { configure, addDecorator } from '@storybook/react'
-import GlobalStyle from '../src/styles/GlobalStyle'
+import { configure } from '@storybook/react'
 
-addDecorator(s => (
-  <React.Fragment>
-    <GlobalStyle />
-    {s()}
-  </React.Fragment>
-))
-
-function loadStories () {
-  const req = require.context('../src', true, /\.storybook\.js$/)
-  req.keys().forEach(filename => req(filename))
-}
-
-configure(loadStories, module)
+// automatically import all files ending in *.stories.js
+configure(require.context('../src', true, /\.stories\.js$/), module)
