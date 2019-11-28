cdab144630f709c58541539f6cf3e464f8c400a6
Eder <edermunhozsantos@gmail.com>
2019-11-27 23:37:31 -0300

    fix: change google font import

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,700&display=swap');
+
   * {
     margin: 0;
     padding: 0;
