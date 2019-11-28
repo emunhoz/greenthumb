458fe6c2a14c9c61bdcc2daae7e05f63745ce2f9
Eder <edermunhozsantos@gmail.com>
2019-11-28 03:45:34 -0300

    feat: add button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import * as S from './styles'
 
-const Button = props => <S.Button {...props} />
+const Button = props => (
+  <S.Button {...props}>{props.icon ? <S.ArrowIcon /> : null}{props.children}</S.Button>
+)
+
+Button.propTypes = {
+  icon: PropTypes.func,
+  children: PropTypes.string
+}
 
 export default Button
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,18 +1,39 @@
 import styled from 'styled-components'
+import { ArrowRight } from 'styled-icons/feather/ArrowRight'
 
 export const Button = styled.button`
-  border: none;
-  border-radius: 0.25em;
-  cursor: pointer;
-  padding: 15px 22px;
-  background: #1885c9;
-  color: white;
+  border: ${({ shape }) => shape === 'line' ? '1px solid var(--primary)' : 'none'};
+  background: ${({ shape }) => shape === 'line' ? 'var(--white)' : 'var(--primary)'};
+  color: ${({ shape }) => shape === 'line' ? 'var(--primary)' : 'var(--white)'};
+  box-sizing: border-box;
+  width: ${({ block }) => block ? '100%' : '170px'};
+  height: 50px;
+  border-radius: 25px;
   font-size: 16px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 300;
+  line-height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    color: ${({ shape }) => shape === 'line' ? 'var(--primary)' : 'var(--white)'};
+  }
+
   &:hover {
     opacity: 0.8;
+    cursor: pointer;
   }
 
   &:disabled {
     opacity: 0.5;
   }
+`
+
+export const ArrowIcon = styled(ArrowRight)`
+  position: absolute;
+  height: 28px;
+  left: 14px;
 `
