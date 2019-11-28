5435a3e927ae2a7db05a21f7eb1fc1ce376f2cb9
Eder <edermunhozsantos@gmail.com>
2019-11-28 03:46:09 -0300

    feat: add label component

---
+++ src/components/Label/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import * as S from './styles'
+
+const Label = props => (
+  <S.Label {...props}>{props.name}</S.Label>
+)
+
+Label.propTypes = {
+  name: PropTypes.string
+}
+
+export default Label
---
+++ src/components/Label/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Label = styled.label`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 27px;
+  color: #6E6E6E;
+
+  &.error {
+    color: var(--danger);
+  }
+`
