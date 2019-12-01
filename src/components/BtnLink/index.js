a1235160e65265850c07c68e35160c2f65d49c57
Eder <edermunhozsantos@gmail.com>
2019-12-01 01:59:05 -0300

    feat: add btn link component

---
+++ src/components/BtnLink/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { ArrowRight, ArrowLeft } from 'styled-icons/feather'
+import * as S from './styles'
+
+const BtnLink = props => (
+  <S.BtnLink {...props}>
+    {props.icon === 'right' ? <S.Icon><ArrowRight /></S.Icon> : null}
+    {props.icon === 'left' ? <S.Icon><ArrowLeft /></S.Icon> : null}
+    {props.children}
+  </S.BtnLink>
+)
+
+BtnLink.propTypes = {
+  icon: PropTypes.string,
+  children: PropTypes.string
+}
+
+export default BtnLink
---
+++ src/components/BtnLink/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+export const BtnLink = styled(Link)`
+  border: ${({ shape }) => shape === 'line' ? '1px solid var(--primary)' : 'none'};
+  background: ${({ shape }) => shape === 'line' ? 'transparent' : 'var(--primary)'};
+  color: ${({ shape }) => shape === 'line' ? 'var(--primary)' : 'var(--white)'};
+  box-sizing: border-box;
+  min-width: ${({ block }) => block ? '100%' : '170px'};
+  min-height: 50px;
+  border-radius: 25px;
+  font-size: 16px;
+  font-family: Montserrat;
+  font-weight: 300;
+  line-height: 20px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  float: left;
+
+  svg {
+    color: ${({ shape }) => shape === 'line' ? 'var(--primary)' : 'var(--white)'};
+  }
+
+  &:hover {
+    opacity: 0.8;
+    cursor: pointer;
+  }
+
+  &:disabled {
+    opacity: 0.5;
+  }
+`
+
+export const Icon = styled.div`
+  position: absolute;
+  left: 10px;
+  svg {
+    height: 28px;
+  }
+`
