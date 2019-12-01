b81d3e363131ab55b2a1343102033bd5a527cdbb
Eder <edermunhozsantos@gmail.com>
2019-12-01 00:07:42 -0300

    style: remove header from main file

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import { store, persistor } from './store'
 import { PersistGate } from 'redux-persist/integration/react'
 import GlobalStyle from './styles/GlobalStyle'
 import Routes from './routes'
-import Header from './components/Header'
 
 const App = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <GlobalStyle />
-      <Header />
       <Routes />
     </PersistGate>
   </Provider>
