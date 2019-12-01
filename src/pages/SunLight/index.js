332216139fa6144566f6f0afbbf6c5385601cf79
Eder <edermunhozsantos@gmail.com>
2019-12-01 01:58:20 -0300

    feat: add first step page

---
+++ src/pages/SunLight/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import StepMessage from '../../components/StepMessage'
+import Card from '../../components/Card'
+import BtnLink from '../../components/BtnLink'
+import { ReactComponent as HighSunlight } from '../../images/icons/coral/high-sun.svg'
+import { ReactComponent as LowSunlight } from '../../images/icons/coral/low-sun.svg'
+import { ReactComponent as NoAnswer } from '../../images/icons/coral/no-answer.svg'
+import Sun from '../../images/illustrations/sun.png'
+import * as S from './styles'
+
+const HomePage = () => (
+  <Layout>
+    <S.Main>
+      <StepMessage img={Sun}>First, set the amount of <strong>sunlight</strong> your plant will get.</StepMessage>
+      <S.Wrapper>
+        <Card icon={HighSunlight} text='High sunlight' color='#FD9872' />
+        <Card icon={LowSunlight} text='Low sunlight' color='#FD9872' />
+        <Card icon={NoAnswer} text='No sunlight' color='#FD9872' />
+        <S.Navs>
+          <BtnLink to='/' icon='left' shape='line'>home</BtnLink>
+          <BtnLink to='/' icon='right' shape='line'>next</BtnLink>
+        </S.Navs>
+      </S.Wrapper>
+    </S.Main>
+  </Layout>
+)
+
+export default HomePage
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Router, Route, Switch } from 'react-router-dom'
 import History from './utils/History'
 import ErrorBoundary from './utils/ErrorBoundary'
 import HomePage from './pages/Home'
+import SunLight from './pages/SunLight'
 
 const Routes = () => (
   <Router history={History}>
     <ErrorBoundary>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/sunlight' component={SunLight} />
         <Route path='*' component={() => <h1>Page not found</h1>} />
       </Switch>
     </ErrorBoundary>
