6b5edf60085ecc6792770246eb4f5547558b453a
Eder <edermunhozsantos@gmail.com>
2019-12-01 00:09:05 -0300

    feat: add header component with logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import * as S from './styled'
+import LogoGreenThumbDesktop from '../../images/logo-desktop.svg'
+import LogoGreenThumb from '../../images/logo-greenthumb.svg'
 
-const Header = () => <S.Header>green thumb</S.Header>
+const Header = () => (
+  <S.Header>
+    <picture>
+      <source srcSet={LogoGreenThumbDesktop} media='(min-width: 768px)' />
+      <S.Logo src={LogoGreenThumb} alt='Logo' />
+    </picture>
+  </S.Header>
+)
 
 export default Header
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,5 +1,20 @@
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
 export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   grid-area: header;
+`
+
+export const Logo = styled.img`
+  width: 166px;
+  height: 20px;
+  margin: 25px 0;
+
+  ${media.greaterThan('medium')`
+    width: auto;
+    height: auto;
+  `}
 `
