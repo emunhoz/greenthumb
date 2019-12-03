7d4871140620e35049e1f9a5260929f5058fcffc
Eder <edermunhozsantos@gmail.com>
2019-12-03 02:22:16 -0300

    perf: remove persistor store

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { store, persistor } from './store'
-import { PersistGate } from 'redux-persist/integration/react'
+import { store } from './store'
 import GlobalStyle from './styles/GlobalStyle'
 import Routes from './routes'
 
 const App = () => (
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <GlobalStyle />
-      <Routes />
-    </PersistGate>
+    <GlobalStyle />
+    <Routes />
   </Provider>
 )
 
