55a7e0370a1c97e4f6aa4423e9a7cbbc94bc40c1
Eder <edermunhozsantos@gmail.com>
2019-12-06 02:47:29 -0300

    perf: improvement pwa on audits

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,41 @@
-self.addEventListener('install', function (event) {})
+var CACHE_NAME = 'greenthumb'
+var urlsToCache = ['/', '/wizard', '/plants']
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opened cache')
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response
+      }
+      return fetch(event.request)
+    })
+  )
+})
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  var cacheWhitelist = ['pwa-task-manager']
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
