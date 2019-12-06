47206578969286b39ec2015921d24f9985dad968
Eder <edermunhozsantos@gmail.com>
2019-12-06 02:19:10 -0300

    feat: add post service

--- src/services/plants.js
+++ src/services/plants.js
@@ -2,7 +2,9 @@ import HTTP_CLIENT from './api'
 
 export async function getPlants (sun, water, pets) {
   try {
-    const resp = await HTTP_CLIENT.get(`?sun=${sun}&water=${water}&pets=${pets}`)
+    const resp = await HTTP_CLIENT.get(
+      `?sun=${sun}&water=${water}&pets=${pets}`
+    )
     return resp
   } catch (error) {
     return error
@@ -12,6 +14,19 @@ export async function getPlants (sun, water, pets) {
 export async function getPlant (id) {
   try {
     const resp = await HTTP_CLIENT.get(`/plant?id=${id}`)
+    return resp
+  } catch (error) {
+    return error
+  }
+}
+
+export async function postPlant (
+  name = 'Raphael Fabeni',
+  email = 'rfabeni@redventures.com',
+  id = '1'
+) {
+  try {
+    const resp = await HTTP_CLIENT.post('/', { name, email, id })
     return resp
   } catch (error) {
     return error
