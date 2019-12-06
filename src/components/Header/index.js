6bc6cc7336c1f5b8ddf0463237f22f33847ee635
Eder <edermunhozsantos@gmail.com>
2019-12-06 02:17:30 -0300

    feat: add link to home on logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import * as S from './styled'
 import LogoGreenThumbDesktop from '../../images/logo-desktop.svg'
 import LogoGreenThumb from '../../images/logo-greenthumb.svg'
+import { Link } from 'react-router-dom'
 
 const Header = () => (
   <S.Header>
-    <picture>
-      <source srcSet={LogoGreenThumbDesktop} media='(min-width: 768px)' />
-      <S.Logo src={LogoGreenThumb} alt='Logo' />
-    </picture>
+    <Link to='/'>
+      <picture>
+        <source srcSet={LogoGreenThumbDesktop} media='(min-width: 768px)' />
+        <S.Logo src={LogoGreenThumb} alt='Logo' />
+      </picture>
+    </Link>
   </S.Header>
 )
 
