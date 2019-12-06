4c344e2b945593502327a1813a9c8eeea02eb724
Eder <edermunhozsantos@gmail.com>
2019-12-05 23:29:19 -0300

    refact: routes

--- src/routes.js
+++ src/routes.js
@@ -3,22 +3,18 @@ import { Router, Route, Switch } from 'react-router-dom'
 import History from './utils/History'
 import ErrorBoundary from './utils/ErrorBoundary'
 import HomePage from './pages/Home'
-import SunLightPage from './pages/SunLight'
-import WaterPage from './pages/Water'
-import DogPage from './pages/Dogs'
-import PickPage from './pages/Pick'
+import PlantsPage from './pages/Plants'
 import DetailsPage from './pages/Details'
+import WizardPage from './pages/Wizard'
 
 const Routes = () => (
   <Router history={History}>
     <ErrorBoundary>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/sunlight' component={SunLightPage} />
-        <Route exact path='/water' component={WaterPage} />
-        <Route exact path='/dogs' component={DogPage} />
-        <Route exact path='/pick' component={PickPage} />
-        <Route exact path='/:id' component={DetailsPage} />
+        <Route path='/wizard' component={WizardPage} />
+        <Route path='/plants' component={PlantsPage} />
+        <Route path='/:id' component={DetailsPage} />
         <Route path='*' component={() => <h1>Page not found</h1>} />
       </Switch>
     </ErrorBoundary>
